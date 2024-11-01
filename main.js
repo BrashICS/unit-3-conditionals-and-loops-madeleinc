d9f708be51aa9ceb1079d32e446aadb58677a50d
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-11-01 14:16:10 -0400

    ...

--- main.js
+++ main.js
@@ -70,55 +70,96 @@ function user(){
 
 //spooky game! 
 function gameStart(){
-    alert("8:21AM\nit's cold. it's dark. the halls are empty. you think you can feel a presence near you, moving all around you, but no one's there.") 
-    alert("8:21AM\nwwas it really a good idea to come to school on halloween?")
-    alert("8:21AM\nyou feel stuck in place. like there's a force keeping you from leaving.")
-    alert("8:21AM\nyou vaguely remember a weird story your friends told you once, about why basically all the students choose not to attend school on halloween, but it's a distant memory now.")
-    alert("8:22AM\nbesides, creepy rumours and holidays will not ruin your perfect attendance!!! (seriously?)")
+    alert("8:23AM\nit's cold. it's dark. the halls are empty. you think you can feel a presence near you, moving all around you, but no one's there.") 
+    alert("8:23AM\nwas it really a good idea to come to school on halloween?")
+    alert("8:23AM\nyou feel stuck in place. like there's a force keeping you from leaving.")
+    alert("8:23AM\nyou vaguely remember a weird story your friends told you once, about why basically all the students choose not to attend school on halloween, but it's a distant memory now.")
+    alert("8:24AM\nbesides, creepy rumours and holidays will not ruin your perfect attendance!!! (seriously?)")
     
-    let playerName = prompt("8:22AM\nsuddenly, a faint voice calls out to you. you still don't see any sign of life in the school.\n\"hello wanderer... what's your name? you don't look like a spirit.\"")
-    alert(`8:22AM\n\"well... welcome ${playerName}, to st. matts HAUNTED high school!\"`)
-    alert("haunted?..")
+    let playerName = prompt("8:24AM\nsuddenly, a faint voice calls out to you. you still don't see any sign of life in the school.\n\"hello wanderer... what's your name? you don't look like a spirit.\"")
+    alert(`8:24AM\n\"well... welcome ${playerName}, to st. matts HAUNTED high school!\"`)
+    alert("8:24AM\nhaunted?..")
     
-    let inputOne = prompt("8:22AM\n\"would you like to go on an adventure with me?\" [YES / NO]")
+    let inputOne = prompt("8:24AM\n\"would you like to go on an adventure with me?\" [YES / NO]")
     if (inputOne == "NO"){
-        alert("8:22AM\n\"umm... okay... let's try that again...\"")
+        alert("8:24AM\n\"umm... okay... let's try that again...\"")
         alert("your mind starts feeling a little foggy...")
         gameStart()
         return;
     }
     if (inputOne == "YES"){
-        alert("8:22AM\n\"yaay! let's begin!\"")
+        alert("8:24AM\n\"yaay! let's begin!\"")
     }
-    gameIntro(playerName);
+    intro(playerName);
 }
 gameStart()
 
-function gameIntro(playerName){
-    let inputOne = prompt(`8:23AM\n\"i need your help, ${playerName}. a looong time ago, i lost some very important items. now, they're scattered all around the school..! can you guess what they are?\"`)
-    alert("8:23AM\n\"...that was a trick question! there's noo possible way you'd know what they are.\"")
-    alert("8:24AM\n\"but you still need to help me! it's impossible for me to collect my belongings now, so you have to find them for me.\"")
-    alert("8:24AM\n\"neither of us can leave the school premise until i get them back. you'll have to do endless math worksheets and eat rotten cafeteria food for the rest of your life.\"")
+function intro(playerName){
+    let inputOne = prompt(`8:25AM\n\"i need your help, ${playerName}. a looong time ago, i lost some very important items. now, they're scattered all around the school..! can you guess what they are?\"`)
+    alert("8:25AM\n\"...that was a trick question! there's noo possible way you'd know what they are.\"")
+    alert("8:25AM\n\"but you still need to help me! it's impossible for me to collect my belongings now, so you have to find them for me.\"")
+    alert("8:26AM\n\"neither of us can leave the school premise until i get them back. you'll have to do endless math worksheets and eat rotten cafeteria food for the rest of your life.\"")
 
-    let inputTwo = prompt(`8:24AM\n\"... so you'll help me, right ${playerName}?\" [YES / NO]`)
+    let inputTwo = prompt(`8:26AM\n\"... so you'll help me, right ${playerName}?\" [YES / NO]`)
     if (inputTwo =="NO"){
-        alert("...")
-        alert("8:24AM\n\"...i'll give you another chance...\"")
+        alert("8:26AM\n...")
+        alert("8:26AM\n\"...i'll give you another chance...\"")
         alert("your mind starts feeling a little foggy...")
-        return gameIntro(playerName);
+        return intro(playerName);
     }
     if (inputTwo =="YES"){
-        alert(`8:25AM\n\"thank you, ${playerName}!\"`)
-        alert("8:25AM\n\"the items i need are 5 green orbs. i know you'll be able to find them. you should have no trouble interacting with them. that's all i can say for now!\"")
-        alert("8:25AM\n\"i promise there's a reward if you help me!\"")
-        alert("8:26AM\n\"well... go on! go find them!\"")
-        alert("8:26AM\nyou don't feel so cold anymore. there was never any physical sign of... whoever you were speaking with... but you can feel that they've disappeared now.")
-        alert("8:26AM\n...shouldn't you be a little more concerned about what just happened though? oddly enough, you can't bring yourself to care.")
-        alert("8:27AM\n as you start walking torwards your second-floor locker, you decide your mysterious new aquaintance and their strange quest is not a priority. you came to school to work, you can focus on finding their green orbs later. (are you really that studious?..)")
-
+        alert(`8:26AM\n\"thank you, ${playerName}!\"`)
+        alert("8:27AM\n\"the items i need are 5 green orbs. i know you'll be able to find them. you should have no trouble interacting with them. that's all i can say for now!\"")
+        alert("8:27AM\n\"i promise there's a reward if you help me!\"")
+        alert("8:27AM\n\"well... go on! go find them!\"")
+        alert("8:27AM\nyou don't feel so cold anymore. there was never any physical sign of... whoever you were speaking with... but you can feel that they've disappeared now.")
+        alert("8:28AM\n...shouldn't you be a little more concerned about what just happened though? oddly enough, you can't bring yourself to care.")
+        transitionOne()
 
     }
+}
+intro()
+
+function transitionOne(){
+    alert("8:28AM\nas you start walking torwards your first period - physics with mr. flynn, you decide your mysterious new aquaintance and their strange quest is not a priority. you came to school to work, you can focus on finding their green orbs later. (are you really that studious?..)")
+    alert("8:28AM\nyou walk past your locker and you realize you need to put away your bag...")
+    alert("8:29AM\nusually, mr flynn is too distracted to notice you walking in with it, and you hide it under your lab bench.")
+    
+    let bag = prompt("8:29AM\ndo you continue walking to first period [1], or put away your bag and risk being late... consequently being shot with his Supersoaker© [2]... ")
+    if (bag == "1"){
+        alert("8:29AM\ngood idea! mr. flynn has been duped before and he'll be duped once again.")
+        mrflynn()
+    }
+    if (bag =="2"){
+        alert("8:29AM\nyou think about how cold that supersoaker water might be and rush to put away your bag...")
+        locker()
+    }
+
+}
+
+function mrflynn(){
+    alert("8:29AM\nmr. flynn's classroom, like the rest of the school, is uncharacteristically quiet. mr. flynn's classroom is never quiet...")
+    alert("8:29AM\nnormally, you'd be able to hear the chatter from your teacher and peers from miles away.")
+    alert("8:29AM\nthe door is closed but unlocked, and you grow increasingly more nervous as you turn the handle...")
+    alert("8:29AM\nyou try to discreetly sneak in, but you realize you've made a grave mistake!")
+    alert("8:29AM\nyou forgot that you're the only student who attended school today! mr. flynn was the only other person in the classroom. he sat idly at his desk looking bored out of his mind.")
+    alert("8:30AM\nmr. flynn looks extremely tired and pale today... almost ghostly. he immediately becomes angry when he noticed you and your bag though, and starts marching torwards you..!")
+    
+    let escape = prompt("8:30AM\nyou need to get out of here... quickly! do you attempt to escape? [YES / NO]")
+    if (escape == "YES"){
+        alert("8:30AM\nyou make a run for it!")
+        alert("8:30AM\nyou might be quick, but mr. flynn's quicker... mr. flynn suddenly appears in front of you, and you realize...")
+        alert("8:30AM\nhe's a ghost!!! (did you forget this school's haunted..?)")
+        alert("8:30AM\nthe ghost of mr. flynn screams in anger and you scream in fear...")
+        alert("your mind starts feeling a little foggy...")
+        transitionOne()
+    }
+
 
 
 }
-gameIntro()
+mrflynn()
+
+function locker(){
+
+}
